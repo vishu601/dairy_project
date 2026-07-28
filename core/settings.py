e94e8cef164de68faaabe11825cbb16b22365829
vishu601 <vishu601@gmail.com>
2026-07-28 10:42:58 +0000

    Fix dashboard template unclosed for tag and update codebase

--- core/settings.py
+++ core/settings.py
@@ -52,7 +52,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
+        default=os.environ.get('DATABASE_URL', f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
         conn_max_age=600
     )
 }
